5561ad1b58533fce8eb4b29c7a8599d93b70f9b4
shreyashettys202 <150761769+shreyashettys202@users.noreply.github.com>
2024-03-17 22:13:37 +0000

    Update jest.config.js

--- WWA-backend-master/jest.config.js
+++ WWA-backend-master/jest.config.js
@@ -1,4 +1,7 @@
-// jest.config.js
 module.exports = {
     testEnvironment: 'node',
+    collectCoverage: true, // Enable coverage collection
+    coverageReporters: ["lcov", "text"], // Specify coverage reporters
+    // Optional: Specify the directory where Jest should output its coverage files
+    coverageDirectory: "coverage",
 };
